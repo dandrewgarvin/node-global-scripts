35ef8d16331ca14a421ac72074364d9a318fa84c
Andrew Garvin <me@andrewgarv.in>
2020-05-29 15:02:51 -0600

    update merge script to use commander. merge script includes checkout and switch arguments

--- merge/index.js
+++ merge/index.js
@@ -15,6 +15,7 @@
  */
 
 const { exec } = require("child_process");
+const { program } = require("commander");
 
 if (!process.argv[2]) {
   console.log("Please pass in the branch name you want to merge from.");
@@ -22,8 +23,28 @@ if (!process.argv[2]) {
   process.exit(127);
 }
 
+program
+  .usage("<name of branch to merge from>")
+  .option("-n, --new <name>", "Name the new branch")
+  .option("-c, --checkout <name>", "checkout to a branch other than current");
+
+program.parse(process.argv);
+
 execPromise("git branch").then(async (result) => {
   let currentBranch = result.split("\n").find((line) => line.match(/\*./g));
+
+  if (program.checkout) {
+    let checkout = result
+      .split("\n")
+      .map((line) => line.trim())
+      .find((line) => line.match(program.checkout));
+
+    if (!checkout) {
+      console.log("Unable to parse checkout branch. Try again later.");
+
+      process.exit(127);
+    }
+  }
 
   if (!currentBranch) {
     console.log("Unable to parse current branch. Try again later.");
@@ -42,8 +63,16 @@ execPromise("git branch").then(async (result) => {
   const pull = await execPromise("git pull");
   console.log(pull);
 
-  console.log("checking back in...");
-  await execPromise(`git checkout ${currentBranch}`);
+  if (program.new) {
+    console.log(`switching to new branch: ${program.new}...`);
+    await execPromise(`git switch -c ${program.new}`);
+  } else if (program.checkout) {
+    console.log(`checking out to ${program.checkout}...`);
+    await execPromise(`git checkout ${program.checkout}`);
+  } else {
+    console.log("checking back in...");
+    await execPromise(`git checkout ${currentBranch}`);
+  }
 
   await execPromise(`git merge ${mergeBranch}`);
   console.log(`successfully merged from ${mergeBranch} into ${currentBranch}`);
