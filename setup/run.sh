b607b803b2922fda691eaee60e05489a297bb158
Andrew Garvin <25071259+dandrewgarvin@users.noreply.github.com>
2022-12-01 16:17:58 -0800

    update setup script

--- setup/run.sh
+++ setup/run.sh
@@ -24,16 +24,17 @@ VOLUME="2.5"
 # SETUP
 echo "Setting machine up..."
 
-sleep 1
+sleep 2
 
 echo "Machine setup complete..."
 
-curl $ASSETS/zelda-chest-sound.mp3 -L -s > /tmp/success.mp3
-
 echo "Congratulations! Closing Process..."
 
+curl $ASSETS/zelda-chest-sound.mp3 -L -s > /tmp/success.mp3
 osascript -e "set Volume $VOLUME" && afplay /tmp/success.mp3
 
 say "Your machine is ready for use, sir"
 
 echo "Goodbye"
+
+sleep 1
