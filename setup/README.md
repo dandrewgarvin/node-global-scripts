6a36dab8e0d434f7955746da6897ccb62eb1baa1
Andrew Garvin <25071259+dandrewgarvin@users.noreply.github.com>
2022-12-01 16:06:47 -0800

    file structure. set up local dev

---
+++ setup/README.md
@@ -0,0 +1,19 @@
+# Setup Script
+
+This script is intended to be ran to set up a new mac computer with a single script. It installs and configures my favorite tools with ease.
+
+## Running Remotely
+
+```
+curl scripts.garvin.dev/setup/run.sh -L | bash
+```
+
+## Running Locally
+
+```bash
+cd ./setup
+
+chmod +x ./run.sh
+
+./run.sh -l
+```
--- setup/run.sh
+++ setup/run.sh
@@ -1,14 +1,37 @@
+DOMAIN="https://scripts.garvin.dev/setup"
+
+# CHECK ENVIRONMENT FOR LOCAL DEV
+# UPDATES DOMAIN BASED ON `-l` FLAG
+
+while getopts ":l" opt; do
+  case $opt in
+    l)
+      DOMAIN="file://$PWD" # The "file://" prefix is required so that we can continue to use `curl` throughout this script
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+
+# CONSTANTS AND VARIABLES
+MODULES="$DOMAIN/modules"
+ASSETS="$DOMAIN/assets"
+
+VOLUME="2.5"
+
+# SETUP
 echo "Setting machine up..."
 
 sleep 1
 
 echo "Machine setup complete..."
 
-curl scripts.garvin.dev/setup/zelda-chest-sound.mp3 -L -s > /tmp/success.mp3
+curl $ASSETS/zelda-chest-sound.mp3 -L -s > /tmp/success.mp3
 
 echo "Congratulations! Closing Process..."
 
-osascript -e 'set Volume 3' && afplay /tmp/success.mp3
+osascript -e "set Volume $VOLUME" && afplay /tmp/success.mp3
 
 say "Your machine is ready for use, sir"
 
